285f913dd69f97414c3a07d0881fb82ab4177b98
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2021-09-10 16:09:18 +0530

    [angular-app] - added functionality for angular app

--- angular-micro-app/src/app/app.component.ts
+++ angular-micro-app/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-micro-app';
+  movieList:any=[];
+  ngOnInit(){
+    const movies:any= localStorage.getItem('movieList');
+     this.movieList=JSON.parse(movies);
+  }
+  onAddToWatchList(movie:any){
+    const tempWatch:any=localStorage.getItem('watchList');
+    const watchList=JSON.parse(tempWatch)
+    localStorage.setItem('watchList',JSON.stringify([
+      ...watchList,
+      movie
+    ]))
+    console.log(movie,"movie",watchList)
+  }
 }
---
+++ movies.js
@@ -0,0 +1,146 @@
+var Movies_List= [
+    {
+      "id": "1",
+      "title": "The Incredible Hulk",
+      "release_date": "2008-06-12",
+      "image":"https://resizing.flixster.com/zd2d1Y7XVOCI71wSFUk2V1xxoOs=/206x305/v2/https://flxt.tmsimg.com/assets/p176337_p_v10_ag.jpg"
+    },
+    {
+      "id": "2",
+      "title": "Iron Man",
+      "release_date": "2008-04-30",
+      "image":"https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+      "id": "3",
+      "title": "Iron Man 2",
+      "release_date": "2010-04-28",
+      "image":"https://images-na.ssl-images-amazon.com/images/I/51xZT2BufOL._SX307_BO1,204,203,200_.jpg"
+    },
+    {
+      "id": "4",
+      "title": "Thor",
+      "release_date": "2011-04-21",
+      "image":"https://movieposters2.com/images/706952-b.jpg"
+    },
+    {
+      "id": "5",
+      "title": "Captain America: The First Avenger",
+      "release_date": "2011-07-22",
+      "image":"https://i.ytimg.com/vi/wsv0Fv-OTIw/movieposter_en.jpg"
+    },
+    {
+      "id": "6",
+      "title": "The Avengers",
+      "release_date": "2012-04-25",
+      "image":"https://images-na.ssl-images-amazon.com/images/I/9152lLsyAaL.jpg"
+    },
+    {
+      "id": "7",
+      "title": "Iron Man 3",
+      "release_date": "2013-04-18",
+      "image":"https://static.wikia.nocookie.net/marvelcinematicuniverse/images/5/55/Iron_Man_3_IMAX_poster.jpg"
+    },
+    {
+      "id": "8",
+      "title": "Thor: The Dark World",
+      "release_date": "2013-10-29",
+      "image":"https://i.ytimg.com/vi/xd7Nz_DBn3o/movieposter_en.jpg"
+    },
+    {
+      "id": "9",
+      "title": "Captain America: The Winter Soldier",
+      "release_date": "2014-03-20",
+      "image":"https://lumiere-a.akamaihd.net/v1/images/p_captainamericathewintersoldier_20531_5668124f.jpeg"
+    },
+    {
+      "id": "10",
+      "title": "Guardians of the Galaxy",
+      "release_date": "2014-07-30",
+      "image":"https://m.media-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_.jpg"
+    },
+    {
+      "id": "11",
+      "title": "Avengers: Age of Ultron",
+      "release_date": "2015-04-22",
+      "image":"https://lumiere-a.akamaihd.net/v1/images/p_avengersageofultron_19907_c62cd1c0.jpeg"
+    },
+    {
+      "id": "12",
+      "title": "Ant-Man",
+      "release_date": "2015-07-14",
+      "image":"https://m.media-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_FMjpg_UX1000_.jpg"
+    },
+    {
+      "id": "13",
+      "title": "Captain America: Civil War",
+      "release_date": "2016-04-27",
+      "image":"https://www.tvguide.com/a/img/catalog/provider/1/2/1-4127271614.jpg"
+    },
+    {
+      "id": "14",
+      "title": "Doctor Strange",
+      "release_date": "2016-10-25",
+      "image":"https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_.jpg"
+    },
+    {
+      "id": "15",
+      "title": "Guardians of the Galaxy Vol. 2",
+      "release_date": "2017-04-19",
+      "image":"https://img1.hotstarext.com/image/upload/f_auto,t_vl/sources/r1/cms/prod/4631/674631-v"
+    },
+    {
+      "id": "16",
+      "title": "Spider-Man: Homecoming",
+      "release_date": "2017-07-05",
+      "image":"https://m.media-amazon.com/images/M/MV5BOGQ5YTM3YzctOTVmMC00OGIyLWFkZTYtMWYwOWZhMjA2MWMwXkEyXkFqcGdeQXVyMjUyMTE5MA@@._V1_.jpg"
+    },
+    {
+      "id": "17",
+      "title": "Thor: Ragnarok",
+      "release_date": "2017-10-25",
+      "image":"https://m.media-amazon.com/images/M/MV5BOGQ5YTM3YzctOTVmMC00OGIyLWFkZTYtMWYwOWZhMjA2MWMwXkEyXkFqcGdeQXVyMjUyMTE5MA@@._V1_.jpg"
+    },
+    {
+      "id": "18",
+      "title": "Black Panther",
+      "release_date": "2018-02-13",
+      "image":"https://lumiere-a.akamaihd.net/v1/images/p_blackpanther_19754_4ac13f07.jpeg"
+    },
+    {
+      "id": "19",
+      "title": "Avengers: Infinity War",
+      "release_date": "2018-04-25",
+      "image":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg"
+    },
+    {
+      "id": "20",
+      "title": "Ant-Man and the Wasp",
+      "release_date": "2018-07-04",
+      "image":"https://m.media-amazon.com/images/M/MV5BYjcyYTk0N2YtMzc4ZC00Y2E0LWFkNDgtNjE1MzZmMGE1YjY1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+    },
+    {
+      "id": "21",
+      "title": "Captain Marvel",
+      "release_date": "2019-03-08",
+      "image":"https://im.rediff.com/movies/2019/mar/06captain-marvel1.jpg"
+    },
+    {
+      "id": "22",
+      "title": "Avengers: Endgame",
+      "release_date": "2019-04-26",
+      "image":"https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg"
+    },
+    {
+      "id": "23",
+      "title": "Spider-Man: Far From Home",
+      "release_date": "2019-07-05",
+      "image":"https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/Spider-Man-FarFromHome-rating.jpg"
+    },
+    {
+      "id": "24",
+      "title": "Spider-Man: Home Run",
+      "release_date": "2021",
+      "image":"https://pbs.twimg.com/media/EksXa0hU8AIBx9n.jpg"
+    }
+  ]
